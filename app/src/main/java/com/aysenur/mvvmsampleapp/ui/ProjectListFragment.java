346096f59a10a262a2f29d2ae5cae54f7ab4dc3b
AysenurGokdemir <aaysenurgokdemir@gmail.com>
2020-10-10 22:33:19 +0300

    Project List Fragment created

--- app/src/main/java/com/aysenur/mvvmsampleapp/ui/ProjectListFragment.java
+++ app/src/main/java/com/aysenur/mvvmsampleapp/ui/ProjectListFragment.java
@@ -1,4 +1,41 @@
 package com.aysenur.mvvmsampleapp.ui;
 
-public class ProjectListFragment {
+import android.os.Bundle;
+
+import com.aysenur.mvvmsampleapp.service.model.Project;
+import com.aysenur.mvvmsampleapp.view.adapter.ProjectAdapter;
+import com.aysenur.mvvmsampleapp.viewmodel.ProjectListViewModel;
+
+import java.util.List;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+public class ProjectListFragment extends Fragment {
+    private ProjectAdapter projectAdapter;
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        final ProjectListViewModel viewModel= ViewModelProviders.of(this).get(ProjectListViewModel.class);
+
+
+    }
+
+    private void observeViewModel(ProjectListViewModel viewModel){
+        // update the list when the data changes
+
+        viewModel.getProjectListObservable().observe(this, new Observer<List<Project>>() {
+            @Override
+            public void onChanged(List<Project> projects) {
+                if (projects !=null){
+
+
+                }
+            }
+        });
+
+    }
 }
