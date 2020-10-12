dccbd02893bf28f922f0c578adfbccab5bc773c1
AysenurGokdemir <aaysenurgokdemir@gmail.com>
2020-10-12 14:33:19 +0300

    AppModule class created

--- app/src/main/java/com/aysenur/mvvmsampleapp/di/AppModule.java
+++ app/src/main/java/com/aysenur/mvvmsampleapp/di/AppModule.java
@@ -1,4 +1,32 @@
 package com.aysenur.mvvmsampleapp.di;
 
+import com.aysenur.mvvmsampleapp.service.repository.GithubService;
+import com.aysenur.mvvmsampleapp.viewmodel.ProjectViewModelFactory;
+
+import javax.inject.Singleton;
+
+import androidx.lifecycle.ViewModelProvider;
+import dagger.Module;
+import dagger.Provides;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+@Module(subcomponents = ViewModelSubComponent.class)  // Something important here to note, do not forget
 class AppModule {
+    @Singleton @Provides
+    GithubService provideGithubService(){
+        return new Retrofit.Builder()
+                .baseUrl(GithubService.HTTPS_API_GITHUB_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(GithubService.class);
+    }
+
+    @Singleton
+    @Provides
+    ViewModelProvider.Factory provideViewModelFactory(
+            ViewModelSubComponent.Builder viewModelSubComponent){
+
+        return new ProjectViewModelFactory(viewModelSubComponent.build());
+    }
 }
