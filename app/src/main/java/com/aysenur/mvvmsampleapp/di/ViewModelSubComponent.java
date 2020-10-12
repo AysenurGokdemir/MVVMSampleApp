f2f749c4dfd4d8a7bcf2bde06906c65e94b47719
AysenurGokdemir <aaysenurgokdemir@gmail.com>
2020-10-12 14:45:35 +0300

    ViewModelSubComponent created

--- app/src/main/java/com/aysenur/mvvmsampleapp/di/ViewModelSubComponent.java
+++ app/src/main/java/com/aysenur/mvvmsampleapp/di/ViewModelSubComponent.java
@@ -1,4 +1,27 @@
 package com.aysenur.mvvmsampleapp.di;
 
+//ViewModel instances oluşturulmasından sorumlu olan ViewModelSubComponent interface .
+
+import com.aysenur.mvvmsampleapp.viewmodel.ProjectListViewModel;
+import com.aysenur.mvvmsampleapp.viewmodel.ProjectViewModel;
+
+import dagger.Subcomponent;
+
+@Subcomponent
 public interface ViewModelSubComponent {
+
+    @Subcomponent.Builder
+    interface Builder{
+        ViewModelSubComponent build();
+    }
+
+    ProjectListViewModel projectListViewModel();
+    ProjectViewModel projectViewModel();
+
+
+
 }
+
+/*** Note that;
+ *      ViewModelSubComponent'in, ViewModel örneklerini almak için ProjectViewModelFactory tarafından çağrılacağını unutmayın.
+ */
