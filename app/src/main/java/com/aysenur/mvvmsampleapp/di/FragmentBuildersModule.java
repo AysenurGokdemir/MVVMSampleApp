db2141b903fc0b05a2892440cf5b309792c666b5
AysenurGokdemir <aaysenurgokdemir@gmail.com>
2020-10-12 14:42:56 +0300

    Fragment Builders Module created

--- app/src/main/java/com/aysenur/mvvmsampleapp/di/FragmentBuildersModule.java
+++ app/src/main/java/com/aysenur/mvvmsampleapp/di/FragmentBuildersModule.java
@@ -1,4 +1,18 @@
 package com.aysenur.mvvmsampleapp.di;
 
+import com.aysenur.mvvmsampleapp.ui.ProjectFragment;
+import com.aysenur.mvvmsampleapp.ui.ProjectListFragment;
+
+import dagger.android.ContributesAndroidInjector;
+
+// Fragments Dagger Module
 public abstract class FragmentBuildersModule {
+
+    //Starting from Dagger 2.10, @ContributesAndroidInjector easily attaches activities and fragments to dagger graph
+    @ContributesAndroidInjector
+    abstract ProjectFragment contributeProjectFragment();
+
+    @ContributesAndroidInjector
+    abstract ProjectListFragment contributeProjectListFragment();
+
 }
